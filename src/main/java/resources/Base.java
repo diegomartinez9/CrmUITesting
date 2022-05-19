336574defc0d5b59c0dc74088929794c9e77fdcd
Diego Martinez <diegomartinez0392@gmail.com>
2022-05-19 11:44:26 -0400

    Se agrega los ultimos metodos de la clase base

--- src/main/java/resources/Base.java
+++ src/main/java/resources/Base.java
@@ -1,7 +1,9 @@
 package resources;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
@@ -9,6 +11,7 @@ import org.openqa.selenium.ie.InternetExplorerDriver;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.time.Duration;
+import java.util.List;
 import java.util.Properties;
 
 public class Base {
@@ -43,5 +46,41 @@ public class Base {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
         return driver;
+    }
+
+    public WebElement findElement (By locator){
+
+        return driver.findElement(locator);
+    }
+
+    public List<WebElement> findElements (By locator) {
+        return driver.findElements(locator);
+    }
+
+    public String getText(WebElement element){
+        return element.getText();
+    }
+
+    public String getText(By locator) {
+        return driver.findElement(locator).getText();
+    }
+
+    public void type(String inputText, By locator) {
+        driver.findElement(locator).sendKeys(inputText);
+    }
+
+    public void click(By locator) {
+        driver.findElement(locator).click();
+    }
+
+    public boolean isDisplayed(By locator){
+        try {
+            return driver.findElement(locator).isDisplayed();
+        }catch (org.openqa.selenium.NoSuchElementException e) {
+                return false;
+        }
+    }
+    public void visit(String url){
+        driver.get(url);
     }
 }
